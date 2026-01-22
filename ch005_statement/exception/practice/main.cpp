2f613286b7c36f0fb046c779c8cf6968decf1f9c
bit <2656585116@qq.com>
2026-01-22 11:58:56 +0800

    [feat]add .vscode.

--- ch005_statement/exception/practice/main.cpp
+++ ch005_statement/exception/practice/main.cpp
@@ -1,6 +1,24 @@
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 
 int main()
 {
+    int dividend{};
+    int divisor{};
+    std::cin >> dividend >> divisor;
 
+    try
+    {
+        if (divisor == 0)
+        {
+            throw std::runtime_error("The divisor cannot be 0.");
+        }
+        std::cout << dividend / divisor;
+    }
+    catch (std::exception err)
+    {
+        std::cout << err.what() << std::endl;
+    }
+    return 0;
 }
